3fd31350e0d736d23b17b9efb89a3ac35d7df713
kazuki-page <92017714+kazuki-page@users.noreply.github.com>
2026-01-22 19:46:15 +0900

    [slug].astroを作成し、そのエラーに合わせてconfig.tsを作った

---
+++ src/content/config.ts
@@ -0,0 +1,10 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      published_date: z.date(),
+    }),
+  }),
+};
