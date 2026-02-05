b35ae09e8f70c5c6a3356007e3171a53762c5e28
kazuki-page <92017714+kazuki-page@users.noreply.github.com>
2026-02-05 19:38:13 +0900

    カテゴリとタグ概念を持たせて、demoにカテゴリをもたせた

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,16 @@ export const collections = {
   posts: defineCollection({
     schema: z.object({
       title: z.string(),
-      published_date: z.date(),
+      slug: z.string(),
+      published_date: z.string(),
+
+      // 記事の性質
+      category: z.enum(["blog", "diary"]),
+
+      // diary 用（weekly / monthly / yearly / 3year）
+      tags: z.array(
+        z.enum(["weekly", "monthly", "yearly", "3year"])
+      ).optional(),
     }),
   }),
 };
