8525c1a86c42bf5a960b4b42bd9b70b96f40d90a
kazuki-page <92017714+kazuki-page@users.noreply.github.com>
2026-02-05 18:35:12 +0900

    トップページを定義、コレクション名をblogをpostsに変更

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from "astro:content";
 
 export const collections = {
-  blog: defineCollection({
+  posts: defineCollection({
     schema: z.object({
       title: z.string(),
       published_date: z.date(),
